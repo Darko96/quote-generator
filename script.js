856b1862de130ea620c62c86743edb8e8f950941
unknown <darkomijatovic2022@gmail.com>
2023-02-09 09:26:33 +0100

    added styles and js code for fatching data

---
+++ script.js
@@ -0,0 +1,26 @@
+let apiQuotes = [];
+
+// Show New Quote
+function newQuote() {
+  // Pick a random quote from apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  console.log(quote);
+}
+
+// Get Quotes From API
+async function getQuotes() {
+  const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+
+  try {
+    const respons = await fetch(apiUrl);
+    apiQuotes = await respons.json();
+
+    newQuote();
+  } catch (error) {
+    // Catch Error Here
+    // alert(error)
+  }
+}
+
+// On Load
+getQuotes();
