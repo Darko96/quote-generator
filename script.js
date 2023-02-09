49ee39801abc5cf895b78f6f4b6ab53de6c9f4d6
unknown <darkomijatovic2022@gmail.com>
2023-02-09 10:23:59 +0100

    added functionality to buttons

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
 let apiQuotes = [];
 
 // Show New Quote
 function newQuote() {
   // Pick a random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  console.log(quote);
+  // Check if Author field is blank and replace it with 'Unknown'
+  if (!quote.author) {
+    // Ako ne postoji quote.author
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+  // Check Quote length to determine styling
+  if (quote.text.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+
+  quoteText.textContent = quote.text;
 }
 
 // Get Quotes From API
@@ -21,6 +41,19 @@ async function getQuotes() {
     // alert(error)
   }
 }
+
+// Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  // Daje nam mogucnost da otvorimo url koji presledimo kao prvi parametar
+  // Dok drugi parametar odredjuje kako ce se otvoriti ta stranica
+  // Sada smo zadali da se otvara u novom prozoru
+  window.open(twitterUrl, "_blank");
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
 
 // On Load
 getQuotes();
